66fef3816faf606176ec76ed8d43c5d435fb6949
jsimonis <jsimonis@student.codam.nl>
2022-02-24 19:59:00 +0100

    Updated Settings.mk to use src/ as include dir

--- Settings.mk
+++ Settings.mk
@@ -1,6 +1,8 @@
 NAME = webserv
 
+INCLUDE_DIRS = src/
+
 CFLAGS += -O3				# I NEED SPEEEED
-#CFLAGS += -g3 -ggdb		# for debugging purposes
+#CFLAGS += -O0 -g3 -ggdb		# for debugging purposes
 #CFLAGS += -D NDEBUG		# remove assert calls
 CFLAGS += -ferror-limit=2	# usually only the first or second errors are usefull, the rest is just junk
