997662cc377a374d21df66ba7fca833911254f5f
Nick Kuipers <nickkuipers@Nicks-MacBook-Pro.local>
2022-02-25 10:29:59 +0100

    Branching out to socket branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 
-int         main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+#include <iostream>
+
+int         main(int argc, char **argv) 
+{
+    std::string conf_filename;
+
+    if (argc != 2) {
+        std::cerr << "Invalid number of arguments." << std::endl;
+        return (1);
+    }
+    conf_filename = argv[1];
     return (0);
 }
