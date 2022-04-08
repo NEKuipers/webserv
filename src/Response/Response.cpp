c051ce747b8ee80139b84c156f075d52d4e145d5
Nick Kuipers <nkuipers@f0r5s6.codam.nl>
2022-04-08 17:09:47 +0200

    Response contains list of codes

--- src/Response/Response.cpp
+++ src/Response/Response.cpp
@@ -13,3 +13,127 @@ Response::Response(const Response &src)
 // {
 //     //add variable assignment
 // }
+
+
+/*
+std::string RequestInterpretor::_getStatusDescription(size_t code)
+{
+	std::map<std::size_t, std::string> m;
+
+	m[100] = "Continue";
+	m[101] = "Switching Protocols";
+	m[200] = "OK";
+	m[201] = "Created";
+	m[202] = "Accepted";
+	m[203] = "Non-Authoritative Information";
+	m[204] = "No Content";
+	m[205] = "Reset Content";
+	m[206] = "Partial Content";
+	m[300] = "Multiple Choices";
+	m[301] = "Moved Permanently";
+	m[302] = "Found";
+	m[303] = "See Other";
+	m[304] = "Not Modified";
+	m[305] = "Use Proxy";
+	m[307] = "Temporary Redirect";
+	m[400] = "Bad Request";
+	m[401] = "Unauthorized";
+	m[402] = "Payment Required";
+	m[403] = "Forbidden";
+	m[404] = "Not Found";
+	m[405] = "Method Not Allowed";
+	m[406] = "Not Acceptable";
+	m[407] = "Proxy Authentication Required";
+	m[408] = "Request Timeout";
+	m[409] = "Conflict";
+	m[410] = "Gone";
+	m[411] = "Length Required";
+	m[412] = "Precondition Failed";
+	m[413] = "Payload Too Large";
+	m[414] = "URI Too Long";
+	m[415] = "Unsupported Media Type";
+	m[416] = "Range Not Satisfiable";
+	m[417] = "Expectation Failed";
+	m[426] = "Upgrade Required";
+	m[500] = "Internal Server Error";
+	m[501] = "Not Implemented";
+	m[502] = "Bad Gateway";
+	m[503] = "Service Unavailable";
+	m[504] = "Gateway Timeout";
+	m[505] = "HTTP Version Not Supported";
+	return m[code];
+	return ("Error");
+}
+*/
+ /*
+ m["aac"] = "audio/aac";
+	m["abw"] = "application/x-abiword";
+	m["arc"] = "application/octet-stream";
+	m["avi"] = "video/x-msvideo";
+	m["azw"] = "application/vnd.amazon.ebook";
+	m["bin"] = "application/octet-stream";
+	m["bmp"] = "image/bmp";
+	m["bz"] = "application/x-bzip";
+	m["bz2"] = "application/x-bzip2";
+	m["csh"] = "application/x-csh";
+	m["css"] = "text/css";
+	m["csv"] = "text/csv";
+	m["doc"] = "application/msword";
+	m["docx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+	m["eot"] = "application/vnd.ms-fontobject";
+	m["epub"] = "application/epub+zip";
+	m["gif"] = "image/gif";
+	m["htm"] = "text/html";
+	m["html"] = "text/html";
+	m["ico"] = "image/x-icon";
+	m["ics"] = "text/calendar";
+	m["jar"] = "application/java-archive";
+	m["jpeg"] = "image/jpeg";
+	m["jpg"] = "image/jpeg";
+	m["js"] = "application/javascript";
+	m["json"] = "application/json";
+	m["mid"] = "audio/midi";
+	m["midi"] = "audio/midi";
+	m["mpeg"] = "video/mpeg";
+	m["mpkg"] = "application/vnd.apple.installer+xml";
+	m["odp"] = "application/vnd.oasis.opendocument.presentation";
+	m["ods"] = "application/vnd.oasis.opendocument.spreadsheet";
+	m["odt"] = "application/vnd.oasis.opendocument.text";
+	m["oga"] = "audio/ogg";
+	m["ogv"] = "video/ogg";
+	m["ogx"] = "application/ogg";
+	m["otf"] = "font/otf";
+	m["png"] = "image/png";
+	m["pdf"] = "application/pdf";
+	m["ppt"] = "application/vnd.ms-powerpoint";
+	m["pptx"] = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+	m["rar"] = "application/x-rar-compressed";
+	m["rtf"] = "application/rtf";
+	m["sh"] = "application/x-sh";
+	m["svg"] = "image/svg+xml";
+	m["swf"] = "application/x-shockwave-flash";
+	m["tar"] = "application/x-tar";
+	m["tif"] = "image/tiff";
+	m["tiff"] = "image/tiff";
+	m["ts"] = "application/typescript";
+	m["ttf"] = "font/ttf";
+	m["vsd"] = "application/vnd.visio";
+	m["wav"] = "audio/x-wav";
+	m["weba"] = "audio/webm";
+	m["webm"] = "video/webm";
+	m["webp"] = "image/webp";
+	m["woff"] = "font/woff";
+	m["woff2"] = "font/woff2";
+	m["xhtml"] = "application/xhtml+xml";
+	m["xls"] = "application/vnd.ms-excel";
+	m["xlsx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+	m["xml"] = "application/xml";
+	m["xul"] = "application/vnd.mozilla.xul+xml";
+	m["zip"] = "application/zip";
+	m["3gp"] = "audio/3gpp";
+	m["3g2"] = "audio/3gpp2";
+	m["7z"] = "application/x-7z-compressed";
+	if (m.count(ext))
+		return (m[ext]);
+	return ("application/octet-stream");
+	*/
