d6e0319dbda268adb651fcd2479a2376980b3b22
Jasper ronald Simonis <jsimonis@f0r4s6.codam.nl>
2022-04-14 13:37:54 +0200

    Made Request class use const references so it does not copy by value, and made the functions const when they dont change anything

--- src/Request/Request.hpp
+++ src/Request/Request.hpp
@@ -21,23 +21,23 @@ class Request
         std::string							body;
         std::string                         plain_request;
         std::vector<std::string>            content_to_lines(std::string req);
-        int                                 parse_header_fields(std::vector<std::string> lines);
+        int                                 parse_header_fields(const std::vector<std::string> &lines);
 
     public:
 		Request();
         Request(const std::string &request_content);
         ~Request();
         Request(const Request &src);
-        Request                             &operator=(const Request &rhs);
-		void								set_request_body(const std::string &buffer);
-        struct request_line                 get_request_line();
-        std::map<std::string, std::string>  get_header_fields();
-        std::string                         get_plain_request();
-        friend std::ostream                 &operator<<(std::ostream &Stream, const Request &request);
-        std::string                         get_header_value(std::string key);
-        void                                parse_requestline(std::string line);
-        bool                                parse_single_header_field(std::string line);
-        int                                 validate_request();
+        Request                             		&operator=(const Request &rhs);
+		void										set_request_body(const std::string &buffer);
+        const struct request_line                 	&get_request_line() const;
+        const std::map<std::string, std::string>	&get_header_fields() const;
+        const std::string                         	&get_plain_request() const;
+        friend std::ostream                 		&operator<<(std::ostream &Stream, const Request &request);
+        const std::string                         	&get_header_value(const std::string& key) const;
+        void                                		parse_requestline(std::string line);
+        bool                                		parse_single_header_field(const std::string& line);
+        int                                 		validate_request();
 
 };
 
